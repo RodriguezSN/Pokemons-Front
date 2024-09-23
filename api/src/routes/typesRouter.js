8d00a70a5ad09e8e984a0e6f20d612c6401a7626
RodriguezSN <gamer.infante51@gmail.com>
2024-09-23 17:04:59 -0300

    separacion del back y el front

--- api/src/routes/typesRouter.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require("express")
-const getTypes = require("../handlers/GetTypes")
-
-const typesRouter = Router()
-
-typesRouter.get("/", getTypes)
-
-module.exports = typesRouter
