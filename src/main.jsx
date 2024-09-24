bd70b6757c8220f71bfb9fc68dfe48a77423f927
RodriguezSN <gamer.infante51@gmail.com>
2024-09-24 07:09:55 -0300

    arreglo en url back

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/store.js";
 
-axios.defaults.baseURL = "https://pokemon-back-production-23c2.up.railway.app/";
+axios.defaults.baseURL = "https://pokemon-back-production-23c2.up.railway.app";
 // axios.defaults.baseURL = "http://localhost:3001";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
